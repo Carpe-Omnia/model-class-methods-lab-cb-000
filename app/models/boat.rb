4988aa33020d7b1ec4c10db1962c5531eef22b01
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-05 06:17:39 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,12 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five 
+    if self.all.size > 5 
+      self.all[0..4]
+    else 
+      self.all 
+    end 
+  end       
 end
